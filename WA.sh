e23d946415908abbc415662d8d802d6769b209a6
Jimmi Setiawan <53130905+MiSetya@users.noreply.github.com>
2019-09-24 11:47:45 +0700

    Add files via upload

---
+++ WA.sh
@@ -0,0 +1,92 @@
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+mer='\033[41;97m' #Tepi
+R='\x1b[1;31m'
+G='\x1b[1;32m'
+B='\x1b[1;34m'
+Y='\x1b[1;33m'
+C='\x1b[1;36m'
+D='\x1b[0m'
+endc='\E[0m'
+enda='\033[0m'
+
+echo
+echo $ku" ___________" $i"Ver. 1.0"
+echo $ku"▕╮╭┻┻╮╭┻┻╮╭▕╮╲"
+echo $ku"▕╯┃╭╮┃┃╭╮┃╰▕╯╭▏" $me"Apk Hacking For Android"
+echo $ku"▕╭┻┻┻┛┗┻┻┛   ╰▏"
+echo $ku"▕╰━━━┓┈┈┈╭╮▕╭╮▏" $pu"Author : MiSetya"
+echo $ku"▕╭╮╰┳┳┳┳╯╰╯▕╰╯▏" $pu"Youtube: MiSetya"
+echo $ku"▕╰╯┈┗┛┗┛┈╭╮▕╮┈▏" $pu"Contact: 0823-8623-4828"
+echo
+echo $me"GUNAKAN UNTUK KEBAIKAN"
+echo
+sleep 3
+echo $pu"[================]"
+echo $pu"[ $me Wait..."
+echo $pu"[================]"
+sleep 4
+echo
+echo $cy" ___________________________"
+echo $cy"|$pu SILAHKAN PILIH APK DIBAWAH|"
+echo $cy"|"
+echo $cy"|"
+echo $cy"| $pu 1) Whatsapp Beta"
+echo $cy"| $pu 2) Kawhatsapp Inmmune 4"
+echo $cy"| $pu 3) Beta Updater (For WA Ori)"
+echo $cy"| $pu 4) Droid Sqli"
+echo $cy"| $pu 5) Termux MOD V2 (anti script abal-abal)"
+echo $cy"| $pu 6) Html Editor"
+echo $cy"|"
+echo $cy"|________"
+read -p "#~Make Your Choice :" pil
+
+case $pil in
+
+1) echo $ku"SILAHKAN BUKA BROWSER UNTUK MENDOWNLOAD"
+sleep 2
+xdg-open https://bercara.com/UyQLzC
+
+;;
+
+2) echo $ku"SILAHKAN BUKA BROWSER UNTUK MENDOWNLOAD"
+sleep 2
+xdg-open https://bercara.com/dfkuuGA58
+
+;;
+
+3) echo $ku"SILAHKAN BUKA BROWSER UNTUK MENDOWNLOAD"
+sleep 2
+xdg-open https://bercara.com/jBUhNJ
+
+;;
+
+4) echo $ku"SILAHKAN BUKA BROWSER UNTUK MENDOWNLOAD"
+sleep 2
+xdg-open https://bercara.com/PwknBiZ3fqpm
+
+;;
+
+5) echo $ku"SILAHKAN BUKA BROWSER UNTUK MENDOWNLOAD"
+sleep 2
+xdg-open https://bercara.com/nY50uOgOW8h
+
+;;
+
+6) echo $ku"SILAHKAN BUKA BROWSER UNTUK MENDOWNLOAD"
+sleep 2
+xdg-open https://bercara.com/3JInC
+
+;;
+
+*) echo $pu"NICE TO MEET YOU!"
+sleep 2
+echo $pu"HOPE TO SEE YOU SOON!"
+sleep 2
+esac
